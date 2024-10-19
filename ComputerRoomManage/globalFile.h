fe964d70cff801e2c7a87d43c51bea1f473d73ed
zehao li <143055238+lizehaoCodes@users.noreply.github.com>
2024-10-19 14:31:26 +0800

    Add files via upload

---
+++ ComputerRoomManage/globalFile.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//管理员文件
+#define Admin_file "admin.txt"
+//学生文件
+#define Student_file "student.txt"
+//教师文件
+#define Teacher_file "teacher.txt"
+//机房信息文件
+#define Computer_file "computerRoom.txt"
+//订单文件
+#define Order_file "order.txt"
